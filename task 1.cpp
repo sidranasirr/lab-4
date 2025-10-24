d43256aa7ca7fed2ee5c8a9843e46495569240aa
sidranasirr <f2025-0792@bnu.edu.pk>
2025-10-24 14:57:57 +0500

    Create task 1.cpp

---
+++ task 1.cpp
@@ -0,0 +1,10 @@
+# lab-4
+#include<iostream>
+using namespace std;
+int main () {
+    cout<<"Name:\tSidra Nasir\n;
+    cout<<"Department:\tSCIT\n";
+    cout<<"Quote;"
+    \T\Learning never exhaust the mind.\"\n";
+    return 0;
+}
